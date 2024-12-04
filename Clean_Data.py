279ef1af9b52680e114454308b568f205ec5edc3
jabellorin05 <133683625+jabellorin05@users.noreply.github.com>
2024-12-04 11:46:39 -0500

    Update Clean_Data.py

--- Clean_Data.py
+++ Clean_Data.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
-import re
 from langdetect import detect, DetectorFactory
 from langdetect.lang_detect_exception import LangDetectException
 import pycountry
